5c9f50a724ffa6c1bbcd6fa524a52c1702f1b4ed
Pablo Borges <31711+pabloborges@users.noreply.github.com>
2022-02-12 13:30:11 -0800

    [Setup] Add setup.sh

--- README.md
+++ README.md
@@ -7,27 +7,25 @@
 
 ## Setup
 
-### Install dependencies and other useful commands
-```sh
-brew install coreutils git git-extras ripgrep starship tldr todo-txt wget zsh zsh-completions zsh-syntax-highlighting
-```
-
 ### Clone repo:
 ```sh
-git clone git@github.com:pabloborges/dotfiles.git ~/dotfiles
+ git clone --recurse-submodules git@github.com:pabloborges/dotfiles.git ~/dotfiles
 ```
 
-### Add symlinks
+### Install the Command Line Tools:
 ```sh
-ln -sf ~/dotfiles/.aliases ~/.aliases
-ln -sf ~/dotfiles/.gemrc ~/.gemrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
-ln -sf ~/dotfiles/config/todo.cfg ~/.todo.cfg
+xcode-select --install
 ```
+
+### Run `brew doctor` and address warnings
+
+### Run setup:
+```sh
+cd ~/dotfiles
+./setup.sh
+```
+
+### Change the font to 'FiraCode Nerd Font Mono'
 
 ### (Optional) Install Solarized Dark Higher Contrast
 The default Solarized Dark theme that comes with iTerm2, doesn't work well with `zsh-completions`.
---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash +e
+
+if ! command -v brew &> /dev/null; then
+  echo "brew could not be found. Install brew and oh-my-zsh and try again"
+  exit 1
+fi
+
+SCRIPT_DIR=$(dirname $0)
+
+echo "Installing font-fira-code-nerd-font"
+brew tap homebrew/cask-fonts
+brew install --quiet font-fira-code-nerd-font
+
+echo "Installing dependencies and other useful commands"
+brew install --quiet coreutils git git-extras neovim ripgrep sourcetree starship tldr todo-txt wget zsh zsh-completions zsh-syntax-highlighting
+
+echo "Adding symlinks"
+ln -sf ~/dotfiles/.aliases ~/.aliases
+ln -sf ~/dotfiles/.gemrc ~/.gemrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
+ln -sf ~/dotfiles/config/todo.cfg ~/.todo.cfg
+ln -sf $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ~/dotfiles/zsh/after/zsh-syntax-highlighting.zsh 
+
+echo "\
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.\
+" > $SCRIPT_DIR/zsh/custom/example.zsh
+
+echo "Your system is ready! Reloading the shell..."
+exec ${SHELL} -l
