5a5bce4d5f0f6089f06da2bb04c070a5aee052dc
Pablo Borges <31711+pabloborges@users.noreply.github.com>
2022-03-04 12:43:12 -0800

    More aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,19 +1,39 @@
-########
-# Alias
-########
+##########
+# Aliases
+##########
+
+# common
+alias l="exa -lah --git --icons"
+alias reload="exec ${SHELL} -l" # Reload the shell
+alias tree="exa --tree --level=2"
 alias ys="yarn start"
+
+# dotfiles
+alias aliases="nvim $0"
+
+# git
+alias gap="git add --patch"
+alias gc="git commit -v"
+alias gcl="git clone --recurse-submodules"
+alias gco="git checkout"
+alias gl="git pull"
+alias glol="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'"
+alias gp="git push -u"
+alias gst="git status"
+
+# projects
+alias notes="nvim ~/Documents/notes/iCloud"
 
 # macOS has no `md5sum`, so use `md5` as a fallback
 command -v md5sum > /dev/null || alias md5sum="md5"
 # macOS has no `sha1sum`, so use `shasum` as a fallback
 command -v sha1sum > /dev/null || alias sha1sum="shasum"
-# Reload the shell (i.e. invoke as a login shell)
-alias reload="exec ${SHELL} -l"
 
 ########
 # Functions
 ########
 command -v gdate > /dev/null && when() { gdate -I -d "$*" }
+notify() { osascript -e "display notification \"$*\"" }
 
 ########
 # Globalias filter (don't expand)
