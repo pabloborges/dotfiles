e53be549d4d12145dd0f09a510d84be2db9cd447
Pablo Borges <31711+pabloborges@users.noreply.github.com>
2025-11-27 16:00:51 -0800

    Update command-not-found handler path

--- zsh/after/command-not-found.zsh
+++ zsh/after/command-not-found.zsh
@@ -1,4 +1,4 @@
-HB_CNF_HANDLER="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
-if [ -f "$HB_CNF_HANDLER" ]; then
-  source "$HB_CNF_HANDLER";
+HOMEBREW_COMMAND_NOT_FOUND_HANDLER="$(brew --repository)/Library/Homebrew/command-not-found/handler.sh"
+if [ -f "$HOMEBREW_COMMAND_NOT_FOUND_HANDLER" ]; then
+  source "$HOMEBREW_COMMAND_NOT_FOUND_HANDLER";
 fi
