b78458eb657017afd21b7249a244b76ed6b382fe
Pablo Borges <31711+pabloborges@users.noreply.github.com>
2026-05-15 17:25:33 -0700

    feat(zsh): bind down arrow to backward history search
    Pairs with the existing UP arrow fzf binding so both arrow keys do
    prefix-aware history navigation.

--- config/zsh/.config/zsh/.zshrc.d/22-fzf.zsh
+++ config/zsh/.config/zsh/.zshrc.d/22-fzf.zsh
@@ -3,3 +3,6 @@ source <(fzf --zsh)
 
 # Bind UP arrow to fzf history search instead of default Ctrl-R
 bindkey '^[[A' fzf-history-widget
+
+# Bind DOWN arrow to default history search (Ctrl-R)
+bindkey '^[[B' history-search-backward
